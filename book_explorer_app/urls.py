92ac96f2d96ba6c041467f1c09105047f1b6e355
Bruno Pagno <brunopagno@usp.br>
2023-05-28 18:53:34 -0300

    feat: book search structure created

--- book_explorer_app/urls.py
+++ book_explorer_app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("search/<slug:query>", views.search, name="search"),
+    path("search/<slug:query>/details/<slug:id>", views.details, name="details"),
 ]
--- book_explorer_app/views.py
+++ book_explorer_app/views.py
@@ -1,5 +1,26 @@
 from django.http import HttpResponse
+import requests
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello, world")
+    return render(request, "books/index.html")
+
+def search(request, query):
+    endpoint = f'https://www.googleapis.com/books/v1/volumes?q={query}'
+    response = requests.get(endpoint).json()
+    total = response.get('totalItems')
+    books = response.get('items')
+    context = {"query": query, "total": total, "books": books, "number_of_books": len(books)}
+    return render(request, "books/search.html", context)
+
+def details(request, query, id):
+    endpoint = f'https://www.googleapis.com/books/v1/volumes/{id}'
+    response = requests.get(endpoint).json()
+    print('response', response)
+    title = response.get('volumeInfo').get('title')
+    description = response.get('volumeInfo').get('description')
+    cover_image_url = response.get('volumeInfo').get('imageLinks').get('smallThumbnail')
+    print()
+    context = {"title": title, "description": description, "cover_image_url": cover_image_url, "query": query }
+    return render(request, "books/details.html", context)
