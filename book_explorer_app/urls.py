1598a97645062aacf436725581cc2e2ea5cb038e
Bruno Pagno <brunopagno@usp.br>
2023-05-30 11:50:42 -0300

    fix: added support for queries with space

--- book_explorer_app/urls.py
+++ book_explorer_app/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("search/<slug:query>", views.search, name="search"),
-    path("search/<slug:query>/details/<slug:id>", views.details, name="details"),
+    path("search", views.search, name="search"),
+    path("search/details/<slug:id>", views.details, name="details"),
 ]
--- book_explorer_app/views.py
+++ book_explorer_app/views.py
@@ -6,21 +6,22 @@ from django.shortcuts import render
 def index(request):
     return render(request, "books/index.html")
 
-def search(request, query):
-    endpoint = f'https://www.googleapis.com/books/v1/volumes?q={query}'
+def search(request):
+    q = request.GET.get('q', '')
+    print('q', q)
+    endpoint = f'https://www.googleapis.com/books/v1/volumes?q={q}'
     response = requests.get(endpoint).json()
     total = response.get('totalItems')
     books = response.get('items')
-    context = {"query": query, "total": total, "books": books, "number_of_books": len(books)}
+    context = {"query": q, "total": total, "books": books, "number_of_books": len(books)}
     return render(request, "books/search.html", context)
 
-def details(request, query, id):
+def details(request, id):
     endpoint = f'https://www.googleapis.com/books/v1/volumes/{id}'
+    q = request.GET.get('q', '')
     response = requests.get(endpoint).json()
-    print('response', response)
     title = response.get('volumeInfo').get('title')
     description = response.get('volumeInfo').get('description')
     cover_image_url = response.get('volumeInfo').get('imageLinks').get('smallThumbnail')
-    print()
-    context = {"title": title, "description": description, "cover_image_url": cover_image_url, "query": query }
+    context = {"title": title, "description": description, "cover_image_url": cover_image_url, "query": q }
     return render(request, "books/details.html", context)
